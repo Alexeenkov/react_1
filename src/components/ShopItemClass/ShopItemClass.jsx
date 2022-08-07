b597235fc751b9574be4ab3e4ab5feacacac8d18
Ilya <alexeenkov-frontand@yandex.ru>
2022-08-07 15:37:29 +0300

    Добавил prop-types и приведение цены к определенному формату

--- src/components/ShopItemClass/ShopItemClass.jsx
+++ src/components/ShopItemClass/ShopItemClass.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import s from './ShopItemClass.module.css';
+import PropTypes from 'prop-types';
 
 class ShopItemClass extends React.Component {
     // eslint-disable-next-line no-useless-constructor
@@ -8,7 +9,7 @@ class ShopItemClass extends React.Component {
     }
 
     render() {
-        const {item} = this.props;
+        const { item } = this.props;
 
         return (
             <div className={s.mainContent}>
@@ -24,7 +25,7 @@ class ShopItemClass extends React.Component {
                 <div className={s.divider}></div>
                 <div className={s.purchaseInfo}>
                     <div className={s.price}>
-                        {item.currency}{item.price}
+                        {item.currency}{Number(item.price).toFixed(2)}
                     </div>
                     <button>Добавить в корзину</button>
                 </div>
@@ -32,5 +33,16 @@ class ShopItemClass extends React.Component {
         );
     }
 }
+
+ShopItemClass.propTypes = {
+    item: PropTypes.shape({
+        brand: PropTypes.string,
+        title: PropTypes.string.isRequired,
+        description: PropTypes.string,
+        descriptionFull: PropTypes.string,
+        price: PropTypes.number.isRequired,
+        currency: PropTypes.string.isRequired,
+    }),
+};
 
 export default ShopItemClass;
--- src/components/ShopItemFunc/ShopItemFunc.jsx
+++ src/components/ShopItemFunc/ShopItemFunc.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import s from './ShopItemFunc.module.css';
+import PropTypes from 'prop-types';
 
 const ShopItemFunc = (props) => {
-    const {item} = props;
+    const { item } = props;
 
     return (
         <div className={s.mainContent}>
@@ -18,12 +19,23 @@ const ShopItemFunc = (props) => {
             <div className={s.divider}></div>
             <div className={s.purchaseInfo}>
                 <div className={s.price}>
-                    {item.currency}{item.price}
+                    {item.currency}{Number(item.price).toFixed(2)}
                 </div>
                 <button>Добавить в корзину</button>
             </div>
         </div>
     );
 }
+
+ShopItemFunc.propTypes = {
+    item: PropTypes.shape({
+        brand: PropTypes.string,
+        title: PropTypes.string.isRequired,
+        description: PropTypes.string,
+        descriptionFull: PropTypes.string,
+        price: PropTypes.number.isRequired,
+        currency: PropTypes.string.isRequired,
+    }),
+};
 
 export default ShopItemFunc;
